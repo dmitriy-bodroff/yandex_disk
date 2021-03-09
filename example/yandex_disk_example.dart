ec4664de8deba855d80c9682d80c6b1db0b8084b
the-english-breaker <>
2021-03-09 22:40:32 +0300

    Сгенерирован скелет библиотеки для будущей интеграции с Яндекс.Диск.

---
+++ example/yandex_disk_example.dart
@@ -0,0 +1,6 @@
+import 'package:yandex_disk/yandex_disk.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/yandex_disk_test.dart
@@ -0,0 +1,16 @@
+import 'package:yandex_disk/yandex_disk.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    Awesome awesome;
+
+    setUp(() {
+      awesome = Awesome();
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
